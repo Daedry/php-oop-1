aff2f399b2327e58eede5bb04407e0a6174c22b2
MyUsername <romamargine@gmail.com>
2022-05-25 17:47:15 +0200

    Add Layout, and more movies

--- index.php
+++ index.php
@@ -31,9 +31,14 @@ class Movie
 }
 
 $movies = [
-    new Movie('Gandahar', 'Gandahar', 'Fantasy', 'Francese', '1988', 'https://ultimavisione.files.wordpress.com/2018/07/gandahar.jpg'),
-    new Movie('Il cielo sopra Berlino', 'Der Himmel uber Berlin', 'Drammatico, Fantasy, Romantico', 'Tedesco', '1987', 'https://upload.wikimedia.org/wikipedia/it/4/4a/Il_cielo_sopra_Berlino.jpg?20091227090915'),
+    new Movie('Gandahar', 'Gandahar', 'Fantasy', 'Francese', '1988', 'https://upload.wikimedia.org/wikipedia/en/thumb/3/39/Gandahar-poster.jpg/220px-Gandahar-poster.jpg'),
+    new Movie('Il cielo sopra Berlino', 'Der Himmel uber Berlin', 'Drammatico, Fantasy, Romantico', 'Tedesco', '1987', 'https://pad.mymovies.it/filmclub/2006/05/384/locandina.jpg'),
     new Movie('La montagna sacra', 'La montaña sagrada', 'Avventura , Drammatico , Fantasy', 'Spagnolo',  '1973', 'https://www.cinefacts.it/foto/h!La_Montagna_Sacra_cinefacts.jpg'),
+    new Movie('L’uomo che venne dalla terra', 'The Man from Earth', 'Drammatico, Fantascienza', 'US',  '2007', 'https://www.ifilmdavedere.it/wpifilm/wp-content/uploads/2011/09/luomo-che-venne-dalla-terra.jpg'),
+    new Movie('Cloud Atlas', 'Cloud Atlas', 'Drammatico, Fantascienza', 'US', '2014', 'https://mr.comingsoon.it/imgdb/locandine/big/48844.jpg'),
+    new Movie('I ORIGINS', 'I ORIGINS', 'Drammatico', 'US', '1987', 'https://mr.comingsoon.it/imgdb/locandine/235x336/52776.jpg'),
+    new Movie('Il pianeta verde', 'La belle verte', 'Fantascienza', 'Francese',  '1996', 'https://m.media-amazon.com/images/I/71TVa51OMfL._SL1024_.jpg'),
+    new Movie('La profezia di celestino', 'The Celestine Prophecy', 'Avventura , Drammatico , Avventura', 'US',  '2006', 'https://pad.mymovies.it/filmclub/2001/09/028/locandina.jpg'),
 ];
 
 // for ($i = 0; $i < count($movies); $i++) {
@@ -49,31 +54,48 @@ $movies = [
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Movie </title>
-    <link rel="stylesheet" href="/assets/style.css">
+    <link rel="stylesheet" href="./assets/style.css">
 </head>
 
 <body>
 
-    <div class="container">
-        <div class="cards">
-            <?php for ($i = 0; $i < count($movies); $i++) : ?>
-                <div class="card">
-                    <div class="card-img"></div>
-                    <div class="card-text">
-                        <h4><?php echo $movies[$i]->title ?></h4>
-                        <h5><?php echo $movies[$i]->genre ?></h5>
-                        <p>
-                            <?php echo $movies[$i]->language ?> <br>
-                            <?php echo $movies[$i]->year ?>
-                        </p>
+    <header>
+        <nav>
+            <h1>Movies</h1>
+        </nav>
+    </header>
+    <!-- /.header -->
+
+    <main>
+        <div class="container">
+            <div class="cards d-flex wrap m-auto">
+                <?php for ($i = 0; $i < count($movies); $i++) : ?>
+                    <div class="card ">
+                        <div class="card-img">
+                            <img src="<?php echo $movies[$i]->image ?>" alt="">
+                        </div>
+                        <div class="card-text">
+                            <h4><?php echo $movies[$i]->title ?></h4>
+                            <h5><?php echo $movies[$i]->genre ?></h5>
+                            <p>
+                                <?php echo $movies[$i]->language ?> <br>
+                                <?php echo $movies[$i]->year ?>
+                            </p>
+                        </div>
                     </div>
                 <?php endfor; ?>
-                </div>
                 <!-- /.card -->
+            </div>
+            <!-- /.cards -->
         </div>
-        <!-- /.cards -->
-    </div>
-    <!-- /.container -->
+        <!-- /.container -->
+    </main>
+    <!-- /.main -->
+
+    <footer>
+        <h2> Original Movies&copy; </h2>
+    </footer>
+    <!-- /.footer -->
 
 </body>
 
